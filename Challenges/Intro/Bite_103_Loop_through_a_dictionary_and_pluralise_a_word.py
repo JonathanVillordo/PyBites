e45a3d548505d011c34ea3fe331a2337a2835503
Jonathan Villordo <jonathanvillordon@gmail.com>
2018-10-05 14:24:02 -0500

    Intro Challenges

---
+++ Challenges/Intro/Bite_103_Loop_through_a_dictionary_and_pluralise_a_word.py
@@ -0,0 +1,15 @@
+games_won = dict(sara=0, bob=1, tim=5, julian=3, jim=1)
+
+
+def print_game_stats(games_won=games_won):
+    """Loop through games_won's dict k, v pairs (items) and
+       print how many games each person has won, pluralize
+       'game' based on number"""
+    for key,value in games_won.items():
+        if value == 1:
+            print("%s has won %s game" %(key,value))
+        else:
+            print("%s has won %s games" %(key,value))
+
+
+print_game_stats()
