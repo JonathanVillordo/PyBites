fd66363f8032d8493c8c2de4b167a7f2c4d7384f
Jonathan Villordo <jonathanvillordon@gmail.com>
2018-10-22 13:30:04 -0500

    Rotate strings

---
+++ Challenges/Begineer/Bite_8_Rotate_string_characters.py
@@ -0,0 +1,20 @@
+import collections
+
+''' Write a function that rotates characters in a string, in
+    bot directions
+'''
+
+def rotate (string, n):
+    """ Rotate characters in a string. Expects string and n (int) for
+    number of characters to move
+     solution 2 return string[n:] + string[:n]
+    """
+    word = collections.deque(string)
+    neword = ''
+
+    for letter in string[:n]:
+        word.popleft()
+        word.append(letter)
+
+
+    return ''.join(word)
